86395862418530a234c2711154b57628f7047ea1
d3trim3ntal <obsidianblackrose@gmail.com>
2022-11-29 12:21:49 -0800

    Continued collectibles
    Revised ruby controller script and created health script

--- RubysAdventureZenteno/Assets/Scripts/RubyController.cs
+++ RubysAdventureZenteno/Assets/Scripts/RubyController.cs
@@ -4,27 +4,50 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public float speed = 3.0f;
+
+    public int maxHealth = 5;
+
+    public int health { get { return currentHealth; } }
+    int currentHealth;
+
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
     // Start is called before the first frame update
     void Start()
     {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
         //Creating the two variables that use the pre built axes
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
 
-        Debug.Log(horizontal);
+    void FixedUpdate()
+    {
         //Creating the movement vector
-        Vector2 position = transform.position;
+        Vector2 position = rigidbody2d.position;
 
         //changing the x and y position
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+
 
         //Set the updated position
-        transform.position = position;
+        rigidbody2d.MovePosition(position);
+    }
+
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
