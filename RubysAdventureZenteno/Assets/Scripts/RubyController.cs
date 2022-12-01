0689e525e204c310b9d11ce012e47fdd6af23a34
d3trim3ntal <obsidianblackrose@gmail.com>
2022-12-01 11:47:20 -0800

    Created new scripts and added collectibles and enemies.
    Finished collectibles and currently have all 4 scripts and enemies and collectibles.

--- RubysAdventureZenteno/Assets/Scripts/RubyController.cs
+++ RubysAdventureZenteno/Assets/Scripts/RubyController.cs
@@ -7,9 +7,13 @@ public class RubyController : MonoBehaviour
     public float speed = 3.0f;
 
     public int maxHealth = 5;
+    public float timeInvincible = 2.0f;
 
-    public int health { get { return currentHealth; } }
+    public int health { get { return currentHealth; }}
     int currentHealth;
+
+    bool isInvincible;
+    float invincibleTimer;
 
     Rigidbody2D rigidbody2d;
     float horizontal;
@@ -29,6 +33,13 @@ public class RubyController : MonoBehaviour
         //Creating the two variables that use the pre built axes
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+        
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
     }
 
     void FixedUpdate()
@@ -45,8 +56,17 @@ public class RubyController : MonoBehaviour
         rigidbody2d.MovePosition(position);
     }
 
-    void ChangeHealth(int amount)
+    public void ChangeHealth(int amount)
     {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
